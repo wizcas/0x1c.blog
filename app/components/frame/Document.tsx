5179712aac46ef44999547c84cc9903578013041
wizcas <chen@0x1c.dev>
2021-12-07 01:37:40 +0800

    icons and manifest and theme colors

--- app/components/frame/Document.tsx
+++ app/components/frame/Document.tsx
@@ -8,10 +8,14 @@ export function Document({
   title?: string;
 }) {
   return (
-    <html lang="en">
+    <html lang="zh">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content="#EB6D71" />
+        <link rel="icon" href="favicon.svg" />
+        <link rel="mask-icon" href="mask-icon.svg" color="#000000" />
+        <link rel="apple-touch-icon" href="logo-light180px.png" />
         {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
