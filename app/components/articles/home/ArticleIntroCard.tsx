5330f9bfe6ce99a929370a19e6383631f1e5c98e
wizcas <chen@0x1c.dev>
2021-12-08 02:36:16 +0800

    anchor card hover state

--- app/components/articles/home/ArticleIntroCard.tsx
+++ app/components/articles/home/ArticleIntroCard.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { Link } from 'remix';
 
 import type { Article } from '~/services/blog/types';
 
@@ -9,21 +10,24 @@ interface Props {
   article: Article;
 }
 export default function ArticleIntroCard({ article }: Props) {
+  const url = `/articles/${article.slug}`;
   return (
-    <article
-      className={classNames(
-        'bg-dark-800 bg-opacity-50',
-        'p-4 rounded-md',
-        'flex flex-col items-stretch',
-        'prose'
-      )}
-    >
-      <h3>{article.title}</h3>
-      <div className="space-x-2">
-        <DateTime value={article.datetime} />
-        {article.topic && <Topic topic={article.topic} />}
-      </div>
-      <p className="line-clamp-5">{article.excerpt}</p>
-    </article>
+    <Link className="card" to={url}>
+      <article
+        className={classNames(
+          'bg-dark-800 bg-opacity-50',
+          'p-4 rounded-md',
+          'flex flex-col items-stretch',
+          'prose'
+        )}
+      >
+        <h3 className="!mt-0">{article.title}</h3>
+        <div className="space-x-2">
+          <DateTime value={article.datetime} />
+          {article.topic && <Topic topic={article.topic} />}
+        </div>
+        <p className="line-clamp-5">{article.excerpt}</p>
+      </article>
+    </Link>
   );
 }
