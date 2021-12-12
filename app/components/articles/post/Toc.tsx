65a2f7fb656142f40ac748cbbc385bec7abc2b1d
wizcas <chen@0x1c.dev>
2021-12-13 01:27:39 +0800

    toc styling

--- app/components/articles/post/Toc.tsx
+++ app/components/articles/post/Toc.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { CSSProperties } from 'react';
 import { Link } from 'remix';
 
 import type { TocItem } from '~/services/blog/types';
@@ -9,19 +10,35 @@ interface Props {
 }
 export default function Toc({ toc, className }: Props) {
   return (
-    <div className={classNames(className)}>
-      <TocList items={toc} />
+    <div className={classNames('text-sm text-light-200', className)}>
+      <TocList items={toc} level={1} />
     </div>
   );
 }
 
-function TocList({ items }: { items: TocItem[] }) {
+function TocList({ items, level }: { items: TocItem[]; level: number }) {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item.href} data-level={item.level}>
-          <Link to={item.href}>{item.text}</Link>
-          {item.children?.length && <TocList items={item.children} />}
+        <li key={item.href}>
+          <Link
+            to={item.href}
+            className={classNames(
+              'quiet',
+              'pl-[var(--offset)] py-1 inline-block',
+              'border-l-4 border-transparent hover:border-hi-link pl-2'
+            )}
+            style={
+              {
+                '--offset': `${(level - 1) * 1 + 0.5}rem`,
+              } as CSSProperties
+            }
+          >
+            {item.text}
+          </Link>
+          {item.children?.length && (
+            <TocList items={item.children} level={level + 1} />
+          )}
         </li>
       ))}
     </ul>
