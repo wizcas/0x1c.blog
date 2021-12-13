6d7c0d28171fe536e612b4d9ae923c9440a87b3b
wizcas <chen@0x1c.dev>
2021-12-14 02:12:05 +0800

    light & dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,17 +52,21 @@ module.exports = {
       blue: colors.sky,
       yellow: colors.amber,
       green: colors.emerald,
-      light: {
-        50: '#F1F3F4',
-        100: '#D5DCDD',
-        200: '#ABB9BA',
-      },
-      dark: {
-        600: '#36494D',
-        700: '#273538',
-        800: '#243033',
-        900: '#1D2729',
-      },
+      primary: colors.rose,
+      // light: {
+      //   50: '#F1F3F4',
+      //   100: '#D5DCDD',
+      //   200: '#ABB9BA',
+      // },
+      // dark: {
+      //   600: '#36494D',
+      //   700: '#273538',
+      //   800: '#243033',
+      //   900: '#1D2729',
+      // },
+      dark: colors.stone,
+      light: colors.zinc,
+      accent: colors.emerald,
       hi: {
         primary: '#EB6D71',
         link: '#52D5E0',
