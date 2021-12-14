6d955e16da21cd797ac03904e36edf09ae6c62cf
wizcas <chen@0x1c.dev>
2021-12-15 01:42:23 +0800

    adjust layout

--- app/components/frame/Footer.tsx
+++ app/components/frame/Footer.tsx
@@ -1,15 +1,18 @@
+import classNames from 'classnames';
+
 const SYMBOL_CLASS = `inline-block text-primary-400 text-sm w-5 not-italic font-semibold text-right mr-2`;
 
 export function Footer() {
   return (
-    <footer className="bg-gray-900 min-h-hero">
+    <footer className="bg-gray-900 min-h-[100px]">
       <div className="page-content py-4 flex flex-col items-center">
         <p>
           <i className={SYMBOL_CLASS}>&copy;</i>
           0x1C.dev
-          <br />
-          <i className={SYMBOL_CLASS}>by</i>
-          Wizcas Chen
+          <i className={classNames(SYMBOL_CLASS, 'mx-2')}>by</i>
+          <a href="mailto:chen@wizcas.is" className="!text-gray-200">
+            Wizcas Chen
+          </a>
         </p>
       </div>
     </footer>
--- app/components/frame/Header.tsx
+++ app/components/frame/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'remix';
 
 import Logo from '~/components/branding/Logo';
 
-const headerStyles = classNames(
+const HEADER_CLASS = classNames(
   'glass',
   'w-full h-12 md:h-20',
   'fixed top-0 left-0 right-0 z-20'
@@ -11,11 +11,15 @@ const headerStyles = classNames(
 
 export default function Header() {
   return (
-    <header className={headerStyles}>
+    <header className={HEADER_CLASS}>
       <div className="page-content flex flex-row items-center justify-between h-full">
         <Link to="/">
           <Logo className="!h-4 md:!h-8 !gap-2 md:!gap-4" />
         </Link>
+        <nav className="space-x-4">
+          <Link to="/">Articles</Link>
+          <Link to="/about">About</Link>
+        </nav>
       </div>
     </header>
   );
