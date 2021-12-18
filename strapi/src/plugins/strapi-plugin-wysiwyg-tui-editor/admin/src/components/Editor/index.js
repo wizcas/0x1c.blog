68af1603ea61bd4d7321ed34e03666bf250a07a1
wizcas <chen@0x1c.dev>
2021-12-19 06:16:14 +0800

    add meta infos by strapi

--- strapi/src/plugins/strapi-plugin-wysiwyg-tui-editor/admin/src/components/Editor/index.js
+++ strapi/src/plugins/strapi-plugin-wysiwyg-tui-editor/admin/src/components/Editor/index.js
@@ -1,13 +1,25 @@
 import React, { useRef, useState } from 'react';
-import { Editor as TuiEditor } from '@toast-ui/react-editor';
 import styled from 'styled-components';
+import {
+  Editor as TuiEditor,
+  Viewer as TuiViewer,
+} from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
-export default function Editor({ onChange, name, value }) {
+export default function Editor({
+  onChange,
+  name,
+  value,
+  required,
+  intlLabel,
+  labelAction,
+  disabled,
+}) {
   const [previewStyle, setPreviewStyle] = useState('vertical');
   const editorRef = useRef(null);
-  function onExpand() {
+  function onToggleStyle() {
     setPreviewStyle((style) => (style === 'vertical' ? 'tab' : 'vertical'));
+    console.log(strapi);
   }
   function onEditorChange() {
     const instance = editorRef.current?.getInstance();
@@ -20,31 +32,54 @@ export default function Editor({ onChange, name, value }) {
   }
   return (
     <div>
-      <FieldLabel>{name}</FieldLabel>
-      <Toolbar>
-        <ToolbarButton onClick={onExpand}>
-          {previewStyle === 'vertical' ? 'Tab Style' : 'Vertical Style'}
-        </ToolbarButton>
-      </Toolbar>
-      <TuiEditor
-        ref={editorRef}
-        usageStatistics={false}
-        initialEditType="markdown"
-        initialValue={value}
-        height="600px"
-        previewStyle={previewStyle}
-        onChange={onEditorChange}
-      />
+      <Header>
+        <FieldLabel>
+          {intlLabel.defaultMessage}
+          {required && <Required>*</Required>}
+        </FieldLabel>
+        {labelAction}
+      </Header>
+      {!disabled ? (
+        <>
+          <Toolbar>
+            <ToolbarButton onClick={onToggleStyle}>
+              {previewStyle === 'vertical' ? 'Tab Style' : 'Vertical Style'}
+            </ToolbarButton>
+          </Toolbar>
+          <TuiEditor
+            ref={editorRef}
+            usageStatistics={false}
+            initialEditType="markdown"
+            initialValue={value}
+            height="600px"
+            previewStyle={previewStyle}
+            onChange={onEditorChange}
+          />
+        </>
+      ) : (
+        <TuiViewer initialValue={value} />
+      )}
     </div>
   );
 }
 
 const FieldLabel = styled.p`
-  text-transform: capitalize;
   font-weight: 600;
   color: #32324d;
   font-size: 0.75rem;
   line-height: 1.33;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  & > *:not(:first-child) {
+    margin-left: 0.25rem;
+  }
+`;
+const Required = styled.span`
+  color: #d02b20;
+  font-size: 0.875rem;
 `;
 
 const Toolbar = styled.div`
