500d5dd5017a20121c34fcce688ae7f2cab4a85e
wizcas <chen@0x1c.dev>
2021-12-21 01:03:22 +0800

    docker file

--- strapi/config/env/production/database.js
+++ strapi/config/env/production/database.js
@@ -1,18 +1,22 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 
-module.exports = () => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  const config = parse(
+    env('BLOG_DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/blog')
+  );
+  return {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
+      client: 'postgres',
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
     },
-  },
-});
+  };
+};
