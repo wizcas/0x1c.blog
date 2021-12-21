649bcc5d8c54081c5334c631c9eac67b76b82df4
wizcas <chen@0x1c.dev>
2021-12-22 01:20:04 +0800

    update the security middleware and upload plugin env control for media library

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -1,11 +1,29 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => {
+  const uploadOrigin = `${env('DO_SPACES_BUCKET')}.${env(
+    'DO_SPACES_ENDPOINT'
+  )}`;
+  return [
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', uploadOrigin],
+            'media-src': ["'self'", 'data:', 'blob:', uploadOrigin],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,30 +1,27 @@
+const s3envs = ['production'];
 module.exports = ({ env }) => {
   const nodeEnv = env('NODE_ENV', 'development');
-  const upload =
-    nodeEnv === 'production'
-      ? {
-          config: {
-            provider: 'aws-s3',
-            providerOptions: {
-              endpoint: env('DO_SPACES_ENDPOINT'),
-              accessKeyId: env('DO_SPACES_ACCESS_KEY'),
-              secretAccessKey: env('DO_SPACES_SECRET_KEY'),
-              params: {
-                Bucket: env('DO_SPACES_BUCKET'),
-              },
+  const upload = s3envs.includes(nodeEnv)
+    ? {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            endpoint: env('DO_SPACES_ENDPOINT'),
+            accessKeyId: env('DO_SPACES_ACCESS_KEY'),
+            secretAccessKey: env('DO_SPACES_SECRET_KEY'),
+            params: {
+              Bucket: env('DO_SPACES_BUCKET'),
             },
           },
-        }
-      : undefined;
-  console.log('ENV', {
-    nodeEnv,
-    endpoint: env('DO_SPACES_ENDPOINT'),
-    accessKeyId: env('DO_SPACES_ACCESS_KEY'),
-    secretAccessKey: env('DO_SPACES_SECRET_KEY'),
-    params: {
-      Bucket: env('DO_SPACES_BUCKET'),
-    },
-  });
+        },
+      }
+    : undefined;
+  const providerName = upload?.config.provider || 'local storage';
+  console.log(`
+================================================
+[${nodeEnv}] using ${providerName} for Media Library
+================================================
+`);
   return {
     upload,
   };
