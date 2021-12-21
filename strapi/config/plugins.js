3090a628ab289db917752de2b74a490baed52ef2
wizcas <chen@0x1c.dev>
2021-12-22 00:46:12 +0800

    set the plugin config for upload on an environmental basis

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,1 +1,31 @@
-module.exports = ({ env }) => ({});
+module.exports = ({ env }) => {
+  const nodeEnv = env('NODE_ENV', 'development');
+  const upload =
+    nodeEnv === 'production'
+      ? {
+          config: {
+            provider: 'aws-s3',
+            providerOptions: {
+              endpoint: env('DO_SPACES_ENDPOINT'),
+              accessKeyId: env('DO_SPACES_ACCESS_KEY'),
+              secretAccessKey: env('DO_SPACES_SECRET_KEY'),
+              params: {
+                Bucket: env('DO_SPACES_BUCKET'),
+              },
+            },
+          },
+        }
+      : undefined;
+  console.log('ENV', {
+    nodeEnv,
+    endpoint: env('DO_SPACES_ENDPOINT'),
+    accessKeyId: env('DO_SPACES_ACCESS_KEY'),
+    secretAccessKey: env('DO_SPACES_SECRET_KEY'),
+    params: {
+      Bucket: env('DO_SPACES_BUCKET'),
+    },
+  });
+  return {
+    upload,
+  };
+};
