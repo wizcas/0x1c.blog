f6e2c9f990d5399f995e77199fecc6bc4b2a3c09
Zhuo Chen <chen@0x1c.dev>
2021-12-24 01:05:03 +0800

    switch to yarn for package management (#9)
    ... and add the main README

---
+++ README.md
@@ -0,0 +1,18 @@
+# 0x1C.dev Blog
+
+This is the implementation of my [blog](https://0x1c.dev). 
+
+Client: [Remix](https://remix.run)
+Server: [Strapi](https://strapi.io)
+
+## Development
+
+- Run `yarn install` in the root directory
+- Run `yarn dev:client` to launch the client development environment
+- Run `yarn dev:server` to launch the server development environment
+- Read the READMEs in the client and server directories respectively to learn more
+- If a warning is shown when starting the client environment about the inconsistent binaries, run this command to fix it:
+  ```shell
+  npm config set scripts-prepend-node-path true
+  ```
+
--- client/app/mocks/categories.ts
+++ client/app/mocks/categories.ts
@@ -9,8 +9,9 @@ export const CATEGORIES: Category[] = [
     id: 'coding-career',
     description: loremIpsum(5),
     themeColor: '#0049B2',
-    coverUrl:
-      'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80',
+    cover: {
+      url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80',
+    },
     articles: generateArticles(3),
   },
   {
@@ -18,8 +19,9 @@ export const CATEGORIES: Category[] = [
     id: 'game-making',
     description: loremIpsum(5),
     themeColor: '#1C1A17',
-    coverUrl:
-      'https://images.unsplash.com/photo-1600861194942-f883de0dfe96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2338&q=80',
+    cover: {
+      url: 'https://images.unsplash.com/photo-1600861194942-f883de0dfe96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2338&q=80',
+    },
     articles: generateArticles(3),
   },
   {
@@ -27,8 +29,9 @@ export const CATEGORIES: Category[] = [
     id: 'bits-pieces',
     description: loremIpsum(5),
     themeColor: '#A3293D',
-    coverUrl:
-      'https://images.unsplash.com/photo-1560088032-d21fe602f4bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+    cover: {
+      url: 'https://images.unsplash.com/photo-1560088032-d21fe602f4bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+    },
     articles: generateArticles(3),
   },
 ];
